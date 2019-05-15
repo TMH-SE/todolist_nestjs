65e3a75e2f95f2ea772979abdaa1b21cb69595cb
Hieu Tran <you@example.com>
2019-05-15 13:48:41 +0700

    commit first

--- src/components/TaskControl.js
+++ src/components/TaskControl.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Row, Icon } from 'antd'
-import TaskSeachControl from './TaskSeachControl'
+import { Button, Row, Icon, Col, Input } from 'antd'
 
 export default class TaskControl extends Component {
   onToggleForm () {
@@ -14,7 +13,13 @@ export default class TaskControl extends Component {
       <div className='task-control'>
         <Button type={'primary'} onClick={this.onToggleForm.bind(this)}><Icon type='plus' /> Add Task</Button>
         <Row className='control'>
-          <TaskSeachControl onSearch={this.onSearch.bind(this)} />
+          <Col span={12} className='searchcontrol'>
+            <Input.Search
+              placeholder='Enter key words'
+              enterButton
+              onSearch={this.onSearch.bind(this)}
+            />
+          </Col>
         </Row>
       </div>
     )
